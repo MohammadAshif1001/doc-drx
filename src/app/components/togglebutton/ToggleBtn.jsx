113e21eb69686acec40daf73209cb6106cf25ac7
arpit <arpit2003y@gmail.com>
2023-09-06 12:06:35 +0530

    minor tweaks

--- src/app/components/togglebutton/ToggleBtn.jsx
+++ src/app/components/togglebutton/ToggleBtn.jsx
@@ -22,7 +22,7 @@ const ToggleBtn = () => {
 
     return (
         <button
-            className={`w-fit absolute right-5 top-2 p-1 outline-1 rounded-lg flex m-1 active:scale-100 duration-200 bg-slate-200 dark:bg-[#0000] `}
+            className={`w-fit absolute right-5 top-2 p-1 outline-none rounded-lg flex m-1 active:scale-100 duration-200 bacground-opacity-0 dark:bg-[#0000] `}
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
             {theme === "light" ? <RiSunLine /> : <RiMoonClearLine />}
